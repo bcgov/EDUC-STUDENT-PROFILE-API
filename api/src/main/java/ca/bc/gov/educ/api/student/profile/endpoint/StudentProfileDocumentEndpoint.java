2e628d255a0510871b8365271ef2788647481639
omprakash mishra <omprakashmishra3978@gmail.com>
2020-04-15 16:07:34 -0700

    updated scopes.

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/endpoint/StudentProfileDocumentEndpoint.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/endpoint/StudentProfileDocumentEndpoint.java
@@ -21,18 +21,18 @@ import static org.springframework.http.HttpStatus.CREATED;
 public interface StudentProfileDocumentEndpoint {
 
   @GetMapping("/{studentProfileID}/documents/{documentID}")
-  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT')")
+  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT_STUDENT_PROFILE')")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"), @ApiResponse(responseCode = "404", description = "NOT FOUND")})
   StudentProfileDocument readDocument(@PathVariable String studentProfileID, @PathVariable String documentID, @RequestParam(value = "includeDocData", defaultValue = "Y") String includeDocData);
 
   @PostMapping("/{studentProfileID}/documents")
-  @PreAuthorize("#oauth2.hasAnyScope('WRITE_DOCUMENT')")
+  @PreAuthorize("#oauth2.hasAnyScope('WRITE_DOCUMENT_STUDENT_PROFILE')")
   @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "CREATED"), @ApiResponse(responseCode = "200", description = "OK")})
   @ResponseStatus(CREATED)
   StudentProfileDocMetadata createDocument(@PathVariable String studentProfileID, @Validated @RequestBody StudentProfileDocument penReqDocument);
 
   @PutMapping("/{studentProfileID}/documents/{documentID}")
-  @PreAuthorize("#oauth2.hasScope('WRITE_DOCUMENT')")
+  @PreAuthorize("#oauth2.hasScope('WRITE_DOCUMENT_STUDENT_PROFILE')")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   StudentProfileDocMetadata updateDocument(@PathVariable UUID studentProfileID, @PathVariable UUID documentID, @Validated @RequestBody StudentProfileDocument penReqDocument);
 
@@ -42,16 +42,16 @@ public interface StudentProfileDocumentEndpoint {
   StudentProfileDocMetadata deleteDocument(@PathVariable String studentProfileID, @PathVariable String documentID);
 
   @GetMapping("/{studentProfileID}/documents")
-  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT')")
+  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT_STUDENT_PROFILE')")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   Iterable<StudentProfileDocMetadata> readAllDocumentMetadata(@PathVariable String studentProfileID);
 
   @GetMapping("/file-requirements")
-  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT_REQUIREMENTS')")
+  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT_REQUIREMENTS_STUDENT_PROFILE')")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   StudentProfileDocRequirement getDocumentRequirements();
 
-  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT_TYPES')")
+  @PreAuthorize("#oauth2.hasScope('READ_DOCUMENT_TYPES_STUDENT_PROFILE')")
   @GetMapping("/document-types")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   Iterable<StudentProfileDocTypeCode> getDocumentTypeCodes();
