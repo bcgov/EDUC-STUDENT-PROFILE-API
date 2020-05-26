0a9bc299e890b3217315bf8304036018b4910621
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-05-26 09:28:20 -0700

    Updates for new build.

---
+++ tools/jenkins/update-configmap.sh
@@ -0,0 +1,72 @@
+envValue=$1
+APP_NAME=$2
+OPENSHIFT_NAMESPACE=$3
+APP_NAME_UPPER=${APP_NAME^^}
+
+TZVALUE="America/Vancouver"
+SOAM_KC_REALM_ID="master"
+KCADM_FILE_BIN_FOLDER="/tmp/keycloak-9.0.3/bin"
+
+oc project $OPENSHIFT_NAMESPACE-$envValue
+
+SOAM_KC_LOAD_USER_ADMIN=$(oc -o json get secret sso-admin-${envValue} | sed -n 's/.*"username": "\(.*\)"/\1/p' | base64 --decode)
+SOAM_KC_LOAD_USER_PASS=$(oc -o json get secret sso-admin-${envValue} | sed -n 's/.*"password": "\(.*\)",/\1/p' | base64 --decode)
+DB_JDBC_CONNECT_STRING=$(oc -o json get configmaps ${APP_NAME}-${envValue}-setup-config | sed -n 's/.*"DB_JDBC_CONNECT_STRING": "\(.*\)",/\1/p')
+DB_PWD=$(oc -o json get configmaps ${APP_NAME}-${envValue}-setup-config | sed -n "s/.*\"DB_PWD_${APP_NAME_UPPER}\": \"\(.*\)\",/\1/p")
+DB_USER=$(oc -o json get configmaps ${APP_NAME}-${envValue}-setup-config | sed -n "s/.*\"DB_USER_${APP_NAME_UPPER}\": \"\(.*\)\"/\1/p")
+SOAM_KC=$OPENSHIFT_NAMESPACE-$envValue.pathfinder.gov.bc.ca
+NATS_CLUSTER=educ_pen_nats_cluster
+NATS_URL="nats://nats.${OPENSHIFT_NAMESPACE}-${envValue}.svc.cluster.local:4222"
+
+oc project $OPENSHIFT_NAMESPACE-tools
+
+###########################################################
+#Fetch the public key
+###########################################################
+$KCADM_FILE_BIN_FOLDER/kcadm.sh config credentials --server https://$SOAM_KC/auth --realm $SOAM_KC_REALM_ID --user $SOAM_KC_LOAD_USER_ADMIN --password $SOAM_KC_LOAD_USER_PASS
+getPublicKey(){
+    executorID= $KCADM_FILE_BIN_FOLDER/kcadm.sh get keys -r $SOAM_KC_REALM_ID | grep -Po 'publicKey" : "\K([^"]*)'
+}
+
+echo Fetching public key from SOAM
+soamFullPublicKey="-----BEGIN PUBLIC KEY----- $(getPublicKey) -----END PUBLIC KEY-----"
+newline=$'\n'
+formattedPublicKey="${soamFullPublicKey:0:26}${newline}${soamFullPublicKey:27:64}${newline}${soamFullPublicKey:91:64}${newline}${soamFullPublicKey:155:64}${newline}${soamFullPublicKey:219:64}${newline}${soamFullPublicKey:283:64}${newline}${soamFullPublicKey:347:64}${newline}${soamFullPublicKey:411:9}${newline}${soamFullPublicKey:420}"
+
+###########################################################
+#Setup for scopes
+###########################################################
+
+#READ_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Read scope for Student Profile\",\"id\": \"READ_STUDENT_PROFILE\",\"name\": \"READ_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#WRITE_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Write scope for Student Profile\",\"id\": \"WRITE_STUDENT_PROFILE\",\"name\": \"WRITE_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#DELETE_DOCUMENT_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Delete scope for Student Profile Document\",\"id\": \"DELETE_DOCUMENT_STUDENT_PROFILE\",\"name\": \"DELETE_DOCUMENT_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#READ_DOCUMENT_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Read scope for Student Profile Document\",\"id\": \"READ_DOCUMENT_STUDENT_PROFILE\",\"name\": \"READ_DOCUMENT_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#READ_DOCUMENT_REQUIREMENTS_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Read scope for Student Profile Document Requirements\",\"id\": \"READ_DOCUMENT_REQ_STUDENT_PROFILE\",\"name\": \"READ_DOCUMENT_REQUIREMENTS_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#WRITE_DOCUMENT_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Write scope for Student Profile Document\",\"id\": \"WRITE_DOCUMENT_STUDENT_PROFILE\",\"name\": \"WRITE_DOCUMENT_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#READ_DOCUMENT_TYPES_STUDENT_PROFILE
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Read scope for Student Profile Document Types\",\"id\": \"READ_DOCUMENT_TYPES_STUDENT_PROFILE\",\"name\": \"READ_DOCUMENT_TYPES_STUDENT_PROFILE\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#READ_STUDENT_PROFILE_STATUSES
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Read scope for Student Profile Statuses\",\"id\": \"READ_STUDENT_PROFILE_STATUSES\",\"name\": \"READ_STUDENT_PROFILE_STATUSES\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#READ_STUDENT_PROFILE_CODES
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"Read scope for Student Profile Codes\",\"id\": \"READ_STUDENT_PROFILE_CODES\",\"name\": \"READ_STUDENT_PROFILE_CODES\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#READ_STUDENT_PROFILE_MACRO
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"SOAM read pen request macro scope\",\"id\": \"READ_STUDENT_PROFILE_MACRO\",\"name\": \"READ_STUDENT_PROFILE_MACRO\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+#WRITE_STUDENT_PROFILE_MACRO
+$KCADM_FILE_BIN_FOLDER/kcadm.sh create client-scopes -r $SOAM_KC_REALM_ID --body "{\"description\": \"SOAM write pen request macro scope\",\"id\": \"WRITE_STUDENT_PROFILE_MACRO\",\"name\": \"WRITE_STUDENT_PROFILE_MACRO\",\"protocol\": \"openid-connect\",\"attributes\" : {\"include.in.token.scope\" : \"true\",\"display.on.consent.screen\" : \"false\"}}"
+
+###########################################################
+#Setup for config-map
+###########################################################
+echo
+echo Creating config map $APP_NAME-config-map 
+oc create -n $OPENSHIFT_NAMESPACE-$envValue configmap $APP_NAME-config-map --from-literal=TZ=$TZVALUE --from-literal=NATS_URL=$NATS_URL --from-literal=NATS_CLUSTER=$NATS_CLUSTER --from-literal=JDBC_URL=$DB_JDBC_CONNECT_STRING --from-literal=ORACLE_USERNAME="$DB_USER" --from-literal=ORACLE_PASSWORD="$DB_PWD" --from-literal=KEYCLOAK_PUBLIC_KEY="$soamFullPublicKey" --from-literal=SPRING_SECURITY_LOG_LEVEL=INFO --from-literal=SPRING_WEB_LOG_LEVEL=INFO --from-literal=APP_LOG_LEVEL=INFO --from-literal=HIBERNATE_STATISTICS=false --from-literal=SPRING_BOOT_AUTOCONFIG_LOG_LEVEL=INFO --from-literal=SPRING_SHOW_REQUEST_DETAILS=false --dry-run -o yaml | oc apply -f -
+echo
+echo Setting environment variables for $APP_NAME-$SOAM_KC_REALM_ID application
+oc project $OPENSHIFT_NAMESPACE-$envValue
+oc set env --from=configmap/$APP_NAME-config-map dc/$APP_NAME-$SOAM_KC_REALM_ID
