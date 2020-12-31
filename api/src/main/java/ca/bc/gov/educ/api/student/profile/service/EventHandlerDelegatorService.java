ef9ae893ae2f604ed0e0225a497e94e55cc137ec
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-12-31 15:47:12 -0800

    loggers...

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/service/EventHandlerDelegatorService.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/service/EventHandlerDelegatorService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EventHandlerDelegatorService {
   public static final String PAYLOAD_LOG = "payload is :: {}";
+  public static final String RESPONDING_BACK_TO_NATS_ON_CHANNEL = "responding back to NATS on {} channel ";
   private final MessagePublisher messagePublisher;
   private final EventHandlerService eventHandlerService;
 
@@ -27,18 +28,21 @@ public class EventHandlerDelegatorService {
           log.info("received UPDATE_STUDENT_PROFILE event :: ");
           log.trace(PAYLOAD_LOG, event.getEventPayload());
           response = eventHandlerService.handleUpdate(event);
+          log.info(RESPONDING_BACK_TO_NATS_ON_CHANNEL, event.getReplyTo());
           messagePublisher.dispatchMessage(event.getReplyTo(), response);
           break;
         case GET_STUDENT_PROFILE:
           log.info("received GET_STUDENT_PROFILE event :: ");
           log.trace(PAYLOAD_LOG, event.getEventPayload());
           response = eventHandlerService.handleGet(event);
+          log.info(RESPONDING_BACK_TO_NATS_ON_CHANNEL, event.getReplyTo());
           messagePublisher.dispatchMessage(event.getReplyTo(), response);
           break;
         case ADD_STUDENT_PROFILE_COMMENT:
           log.info("received ADD_STUDENT_PROFILE_COMMENT event :: ");
           log.trace(PAYLOAD_LOG, event.getEventPayload());
           response = eventHandlerService.handleAddComment(event);
+          log.info(RESPONDING_BACK_TO_NATS_ON_CHANNEL, event.getReplyTo());
           messagePublisher.dispatchMessage(event.getReplyTo(), response);
           break;
         default:
