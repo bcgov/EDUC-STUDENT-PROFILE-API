09a8e3eb762ce8aaa0af033998e9b63bdec8e21b
OMPRAKASH MISHRA <32200996+mishraomp@users.noreply.github.com>
2021-05-27 17:41:22 -0700

    Update StudentProfileInterceptor.java

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/config/StudentProfileInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/config/StudentProfileInterceptor.java
@@ -41,7 +41,7 @@ public class StudentProfileInterceptor extends HandlerInterceptorAdapter {
   @Override
   public void afterCompletion(@NonNull HttpServletRequest request, HttpServletResponse response, @NonNull Object handler, Exception ex) {
     int status = response.getStatus();
-    if(status >= 200 && status < 300) {
+    if(status == 404 || (status >= 200 && status < 300)) {
       log.info("RESPONSE STATUS: {}", status);
     } else {
       log.error("RESPONSE STATUS: {}", status);
