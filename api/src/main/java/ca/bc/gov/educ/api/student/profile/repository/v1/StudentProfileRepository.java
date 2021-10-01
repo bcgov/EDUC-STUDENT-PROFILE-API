5214da0ba91a0dde9e950de8d54c5dc15789b61b
Mingwei <mingwei.y@gmail.com>
2021-10-01 12:13:28 -0700

    Fix: Remove unused code

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/repository/v1/StudentProfileRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/repository/v1/StudentProfileRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
